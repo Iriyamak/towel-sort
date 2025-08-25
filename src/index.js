55b000cf0a4a31fb70a3c5020adeec59132d56d5
Iryna Moiseieva <iriyamois@gmail.com>
2025-08-25 16:49:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (!matrix || matrix.length === 0) return [];
+
+  let result = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    if (i % 2 === 0) {
+      // чётная строка — как есть
+      result.push(...matrix[i]);
+    } else {
+      // нечётная строка — реверс
+      result.push(...matrix[i].reverse());
+    }
+  }
+
+  return result;
 };
