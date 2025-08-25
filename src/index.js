4faa446d3913c804ab5dce1d34c7a9fc422e1b2e
Iryna Moiseieva <iriyamois@gmail.com>
2025-08-25 16:54:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 module.exports = function towelSort(matrix) {
-  if (!matrix || matrix.length === 0) return [];
+if (!matrix || matrix.length === 0) return [];
 
-  let result = [];
+  const result = [];
 
-  for (let i = 0; i < matrix.length; i++) {
+  for (let i = 0; i < matrix.length; i += 1) {
     if (i % 2 === 0) {
-      // чётная строка — как есть
       result.push(...matrix[i]);
     } else {
-      // нечётная строка — реверс
       result.push(...matrix[i].reverse());
     }
   }
